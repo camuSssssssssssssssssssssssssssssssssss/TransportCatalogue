710db2e15c3d99cefb02007abc91f92de0b785e5
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-17 11:31:45 +0400

    Update stat_reader.h

--- stat_reader.h
+++ stat_reader.h
@@ -1,1 +1,5 @@
+#pragma once
+#include "transport_catalogue.h"
+#include <iostream>
 /*stat_reader.h, stat_reader.cpp — чтение запросов на вывод и сам вывод;*/
+void ProcessRequests(TransportCatalogue& catalogue);
