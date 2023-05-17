18a43cbdcbb21facf6186171ab204856b036ffc3
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-17 20:03:21 +0400

    Работает

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -4,9 +4,9 @@
 
 #include <deque>
 #include <string>
+#include <iostream>
 #include <unordered_map>
 #include <vector>
-#include <optional>
 #include <stdexcept>
 #include <unordered_set>
 
@@ -29,11 +29,11 @@ struct RouteInfo {
 
 class TransportCatalogue {
 public:
-    void AddRoute(const std::string& route_number, const std::vector<std::string>& route_stops, bool circular_route);
-    void AddStop(const std::string& stop_name, Coordinates& coordinates);
-    const Bus* FindRoute(const std::string& route_number) const;
-    const Stop* FindStop(const std::string& stop_name) const;
-    const RouteInfo RouteInformation(const std::string& route_number) const;
+    void AddBusRoute(const std::string& route_number, const std::vector<std::string>& route_stops, bool circular_route);
+    void AddBusStop(const std::string& stop_name, Coordinates& coordinates);
+    const Bus* FindBusRoute(const std::string& route_number) const;
+    const Stop* FindBusStop(const std::string& stop_name) const;
+    const RouteInfo BusRouteInformation(const std::string& route_number) const;
     size_t UniqueStopsCount(const std::string& route_number) const;
 
 private:
