f4c57359243742ed85e067c1131f6c45a79aafc7
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-17 20:03:58 +0400

    Работает

--- stat_reader.cpp
+++ stat_reader.cpp
@@ -2,18 +2,18 @@
 
 #include <iomanip>
 
-void ProcessRequests(TransportCatalogue& catalogue) {
+void OutTransport(TransportCatalogue& catalogue) {
     size_t requests_count;
     std::cin >> requests_count;
     for (size_t i = 0; i < requests_count; ++i) {
         std::string line;
         std::cin >> line;
-        if (line.empty()) 
+        if (line.empty())
             break;
         std::getline(std::cin, line);
         std::string route_number = line.substr(1, line.npos);
-        if (catalogue.FindRoute(route_number)) {
-            std::cout << "Bus " << route_number << ": " << catalogue.RouteInformation(route_number).stops_count << " stops on route, " << catalogue.RouteInformation(route_number).unique_stops_count << " unique stops, " << catalogue.RouteInformation(route_number).route_length << " route length\n";
+        if (catalogue.FindBusRoute(route_number)) {
+            std::cout << "Bus " << route_number << ": " << catalogue.BusRouteInformation(route_number).stops_count << " stops on route, " << catalogue.BusRouteInformation(route_number).unique_stops_count << " unique stops, " << catalogue.BusRouteInformation(route_number).route_length << " route length\n";
         }
         else {
             std::cout << "Bus " << route_number << ": not found\n";
