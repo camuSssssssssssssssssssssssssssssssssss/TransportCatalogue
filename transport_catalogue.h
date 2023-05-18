c1a4d42dddc401b1702c5c2413c6c957d617e428
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-18 16:25:44 +0400

    update

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -8,7 +8,9 @@
 #include <unordered_map>
 #include <vector>
 #include <stdexcept>
+#include <set>
 #include <unordered_set>
+
 
 struct Bus {
     std::string number;
@@ -35,10 +37,36 @@ public:
     const Stop* FindBusStop(const std::string& stop_name) const;
     const RouteInfo BusRouteInformation(const std::string& route_number) const;
     size_t UniqueStopsCount(const std::string& route_number) const;
+    std::set<std::string>BusToStop(const std::string& stop_name) const;
 
 private:
     std::deque<Bus> buses_;
     std::deque<Stop> stops_;
     std::unordered_map<std::string_view, const Bus*> finderbus_;
     std::unordered_map<std::string_view, const Stop*> finderstop_;
+    std::unordered_map<std::string, std::set<std::string>>bustoforstop;
+    
+
+        /*
+        
+        Bus 256
+        Bus 750
+        Bus 751
+
+        Stop Samara
+        Stop Prazhskaya
+        Stop Biryulyovo Zapadnoye
+        
+        */
+  
+    /*
+    Bus 256: 6 stops on route, 5 unique stops, 4371.02 route length
+    Bus 750: 5 stops on route, 3 unique stops, 20939.5 route length
+    Bus 751: not found
+
+    Stop Samara : not found
+    Stop Prazhskaya : no buses
+    Stop Biryulyovo Zapadnoye : buses 256 828
+    
+    */
 };
