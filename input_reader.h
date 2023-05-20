5f0ce0871bca11cecdbd309b9b764eaa3214fe64
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-20 18:57:43 +0400

    Update input_reader.h

--- input_reader.h
+++ input_reader.h
@@ -7,7 +7,9 @@
 
 /*input_reader.h, input_reader.cpp — чтение запросов на заполнение базы;*/
 
-void FillCatalogue(std::istream& in, TransportCatalogue& catalogue);
+void FillCatalogue(TransportCatalogue&);
 
+void ParseStop(TransportCatalogue&, std::string);
+void ParseBus(TransportCatalogue&, std::string);
 std::pair<std::vector<std::string>, bool> FillRoute(std::string& line);
 
