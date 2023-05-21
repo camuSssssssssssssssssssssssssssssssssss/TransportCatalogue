16230f016425155300a1a523a3de03d2951d01b2
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-21 16:48:29 +0400

    Update stat_reader.h

--- stat_reader.h
+++ stat_reader.h
@@ -3,3 +3,5 @@
 #include <iostream>
 /*stat_reader.h, stat_reader.cpp — чтение запросов на вывод и сам вывод;*/
 void OutTransport(TransportCatalogue& catalogue);
+void OutBus(TransportCatalogue&, std::string);
+void OutStop(TransportCatalogue&, std::string);
