ca52439c941ec35553632af73bdbb0de340da4f3
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-21 16:40:50 +0400

    Update stat_reader.cpp

--- stat_reader.cpp
+++ stat_reader.cpp
@@ -6,35 +6,46 @@ void OutTransport(TransportCatalogue& catalogue) {
     size_t requests_count;
     std::cin >> requests_count;
     for (size_t i = 0; i < requests_count; ++i) {
-        std::string line, key;
-        std::cin >> key;
+        std::string keyword, line;
+        std::cin >> keyword;
         std::getline(std::cin, line);
-        if (key == "Bus") {
-            std::string route_number = line.substr(1, line.npos);
-            if (catalogue.FindBusRoute(route_number)) {
-                std::cout << "Bus " << route_number << ": " << catalogue.BusRouteInformation(route_number).stops_count << " stops on route, " << catalogue.BusRouteInformation(route_number).unique_stops_count << " unique stops, " << catalogue.BusRouteInformation(route_number).route_length << " route length\n";
-            }
-            else {
-                std::cout << "Bus " << route_number << ": not found\n";
-            }
+        if (keyword == "Bus") {
+            OutBus(catalogue, line);
         }
+        if (keyword == "Stop") {
+            OutStop(catalogue, line);
+        }
+    }
+}
 
-        else if (key == "Stop") {
-            std::string stopinfo = line.substr(1, line.npos);
-            if (!catalogue.FindBusStop(stopinfo))
-                std::cout << "Stop: " << stopinfo << ": not found" << std::endl;
-            else {
-                if (catalogue.BusToStop(stopinfo).empty()) {
-                    std::cout << "Stop: " << stopinfo << ": no buses" << std::endl;
-                }
-                else {
-                    std::cout << "Stop: " << stopinfo << ": buses ";
-                    for (const auto& elsestop : catalogue.BusToStop(stopinfo)) {
-                        std::cout << elsestop << " ";
-                    }
-                    std::cout << std::endl;
-                }
+void OutBus(TransportCatalogue& ctlg, std::string inf) {
+    std::string route_number = inf.substr(1, inf.npos);
+    if (ctlg.FindBusRoute(route_number)) {
+        std::cout << "Bus " << route_number << ": " << ctlg.BusRouteInformation(route_number).stops_count << " stops on route, "
+            << ctlg.BusRouteInformation(route_number).unique_stops_count << " unique stops, " << ctlg.BusRouteInformation(route_number).route_length << " route length, "
+            << ctlg.BusRouteInformation(route_number).curvature << " curvature\n";
+    }
+    else {
+        std::cout << "Bus " << route_number << ": not found\n";
+    }
+}
+
+
+void OutStop(TransportCatalogue& ctlg, std::string inf){
+    if (ctlg.FindBusStop(inf) == nullptr) {
+        std::cout << "Stop " << inf << ": not found" << std::endl;
+    }
+    else {
+        auto a = ctlg.BusToStop(inf);
+        if (a.empty()) {
+            std::cout << "Stop " << inf << ": no buses" << std::endl;
+        }
+        else {
+            std::cout << "Stop " << inf << ": buses ";
+            for (const auto& bus : a) {
+                std::cout << bus << " ";
             }
+            std::cout << std::endl;
         }
     }
 }
