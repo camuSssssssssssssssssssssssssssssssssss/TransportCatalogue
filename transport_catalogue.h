5d1298ee36ee8e21faf1d07b54808fb5057c8845
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-30 20:16:05 +0400

    Update transport_catalogue.h

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -13,44 +13,47 @@
 #include <functional>
 #include <unordered_set>
 
-
-struct Bus {
-    std::string number;
-    std::vector<std::string> stops;
-    bool circular_route;
-};
-
-
-struct Stop {
-    std::string name;
-    Coordinates coordinates;
-    std::unordered_map<std::string, int> stop_distances;
-};
-
-struct RouteInfo {
-    size_t stops_count;
-    size_t unique_stops_count;
-    double route_length;
-    double curvature;
-};
+namespace infostruct {
+    struct Bus {
+        std::string number;
+        std::vector<std::string> stops;
+        bool circular_route;
+    };
 
 
-class TransportCatalogue {
-public:
-    void AddBusRoute(Bus&);
-    void AddBusStop(Stop&);
-    const Bus* FindBusRoute(const std::string& route_number) const;
-    Stop* FindBusStop(const std::string& stop_name) const;
-    const RouteInfo BusRouteInformation(const std::string& route_number) const;
-    size_t UniqueStopsCount(const std::string& route_number) const;
-    std::set<std::string>BusToStop(const std::string& stop_name) const;
-    void SetStopDistance(Stop* from, Stop* to, int distance);
-    int GetStopDistance(const Stop* from, const Stop* to) const;
+    struct Stop {
+        std::string name;
+        coordinate::Coordinates coordinates;
+        std::unordered_map<std::string, int> stop_distances;
+    };
 
-private:
-    std::deque<Bus> buses_;
-    std::deque<Stop> stops_;
-    std::unordered_map<std::string_view, const Bus*> finderbus_;
-    std::unordered_map<std::string_view, Stop*> finderstop_;
-    std::unordered_map<std::string, std::set<std::string>> bustoforstop_;
-};
+    struct RouteInfo {
+        size_t stops_count;
+        size_t unique_stops_count;
+        double route_length;
+        double curvature;
+    };
+}
+
+
+namespace infocatalogueclass {
+    class TransportCatalogue {
+    public:
+        void AddBusRoute(const infostruct::Bus&);
+        void AddBusStop(const infostruct::Stop&);
+        const infostruct::Bus* FindBusRoute(const std::string&) const;
+        infostruct::Stop* FindBusStop(const std::string&) const;
+        const infostruct::RouteInfo BusRouteInformation(const std::string&) const;
+        size_t UniqueStopsCount(const std::string&) const;
+        std::set<std::string>BusToStop(const std::string&) const;
+        void SetStopDistance(infostruct::Stop*, infostruct::Stop*, int);
+        int GetStopDistance(const infostruct::Stop*, const infostruct::Stop*) const;
+
+    private:
+        std::deque<infostruct::Bus> buses_;
+        std::deque<infostruct::Stop> stops_;
+        std::unordered_map<std::string_view, const infostruct::Bus*> finderbus_;
+        std::unordered_map<std::string_view, infostruct::Stop*> finderstop_;
+        std::unordered_map<std::string, std::set<std::string>> bustoforstop_;
+    };
+}
