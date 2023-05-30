1427a32d0fab47ef8cc31a3f4a1808d4d93d0c77
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-30 20:13:47 +0400

    Update input_reader.cpp

--- input_reader.cpp
+++ input_reader.cpp
@@ -4,56 +4,56 @@
 #include <vector>
 #include <cmath>
 
-void FillTransportCatalogue(TransportCatalogue& catalogue) {
-    
-    size_t requests_count;
+void fill::TransportCatalogue(std::istream& in, infocatalogueclass::TransportCatalogue& ctlg) {
+    size_t count;
 
     std::vector<std::string> buses_;
     std::vector<std::string> stops_;
     std::vector<std::string> stop_distances_;
 
-    std::cin >> requests_count;
-    for (size_t i = 0; i < requests_count; ++i) {
-        std::string keyword, line;
-        std::cin >> keyword;
-        std::getline(std::cin, line);
+    in >> count;
+    for (size_t i = 0; i < count; ++i) {
+        std::string keyword, inf;
+        in >> keyword;
+        std::getline(in, inf);
         if (keyword == "Stop") {
-            stops_.push_back(line);
+            stops_.push_back(inf);
         }
         if (keyword == "Bus") {
-            buses_.push_back(line);
+            buses_.push_back(inf);
         }
     }
     stop_distances_ = stops_;
     for (auto& stop_ : stops_) {
-        Stop stop = ParseStop(stop_);
-        catalogue.AddBusStop(stop);
+        infostruct::Stop stop = ParseStop(stop_);
+        ctlg.AddBusStop(stop);
     }
     for (auto& stop_ : stop_distances_) {
-        AddStopDistances(stop_, catalogue);
+        AddStopDistances(stop_, ctlg);
     }
     for (auto& bus_ : buses_) {
-        Bus bus = ParseBus(bus_);
-        catalogue.AddBusRoute(bus);
+        infostruct::Bus bus = ParseBus(bus_);
+        ctlg.AddBusRoute(bus);
         bus = {};
     }
 }
 
-Stop ParseStop(std::string& line) {
-    Stop stop;
-    std::string stop_name = line.substr(1, line.find_first_of(':') - line.find_first_of(' ') - 1);
-    double lat = std::stod(line.substr(line.find_first_of(':') + 2, line.find_first_of(',') - 1));
-    double lng;
-    line.erase(0, line.find_first_of(',') + 2);
-    if (line.find_last_of(',') == line.npos) {
-        lng = std::stod(line.substr(0, line.npos - 1));
-        line.clear();
+infostruct::Stop fill::ParseStop(std::string& inf) {
+    infostruct::Stop stop;
+    std::string stop_name = inf.substr(1, inf.find_first_of(':') - inf.find_first_of(' ') - 1);
+
+    double latitude = std::stod(inf.substr(inf.find_first_of(':') + 2, inf.find_first_of(',') - 1));
+    double longitude;
+    inf.erase(0, inf.find_first_of(',') + 2);
+    if (inf.find_last_of(',') == inf.npos) {
+        longitude = std::stod(inf.substr(0, inf.npos - 1));
+        inf.clear();
     }
     else {
-        lng = std::stod(line.substr(0, line.find_first_of(',')));
-        line.erase(0, line.find_first_of(',') + 2);
+        longitude = std::stod(inf.substr(0, inf.find_first_of(',')));
+        inf.erase(0, inf.find_first_of(',') + 2);
     }
-    Coordinates stop_coordinates = { lat, lng };
+    coordinate::Coordinates stop_coordinates = { latitude, longitude };
 
     stop.name = stop_name;
     stop.coordinates = stop_coordinates;
@@ -61,20 +61,20 @@ Stop ParseStop(std::string& line) {
     return stop;
 }
 
-Bus ParseBus(std::string& line) {
-    Bus bus_info;
+infostruct::Bus fill::ParseBus(std::string& inf) {
+    infostruct::Bus bus_info;
     std::vector<std::string> route_stops;
-    std::string route_number = line.substr(1, line.find_first_of(':') - 1);
-    line.erase(0, line.find_first_of(':') + 2);
+    std::string route_number = inf.substr(1, inf.find_first_of(':') - 1);
+    inf.erase(0, inf.find_first_of(':') + 2);
     bool circular_route = false;
     std::string stop_name;
-    auto pos = line.find('>') != line.npos ? '>' : '-';
-    while (line.find(pos) != line.npos) {
-        stop_name = line.substr(0, line.find_first_of(pos) - 1);
+    auto pos = inf.find('>') != inf.npos ? '>' : '-';
+    while (inf.find(pos) != inf.npos) {
+        stop_name = inf.substr(0, inf.find_first_of(pos) - 1);
         route_stops.push_back(stop_name);
-        line.erase(0, line.find_first_of(pos) + 2);
+        inf.erase(0, inf.find_first_of(pos) + 2);
     }
-    stop_name = line.substr(0, line.npos - 1);
+    stop_name = inf.substr(0, inf.npos - 1);
     route_stops.push_back(stop_name);
     if (pos == '>') circular_route = true;
 
@@ -84,35 +84,34 @@ Bus ParseBus(std::string& line) {
     return bus_info;
 }
 
-void AddStopDistances(std::string& line, TransportCatalogue& catalogue) {
-    if (!line.empty()) {
-        std::string stop_from_name = ParseStop(line).name;
-        Stop* from = catalogue.FindBusStop(stop_from_name);
+void fill::AddStopDistances(std::string& inf, infocatalogueclass::TransportCatalogue& ctlg) {
+    if (!inf.empty()) {
+        std::string stop_from_name = ParseStop(inf).name;
+        infostruct::Stop* from = ctlg.FindBusStop(stop_from_name);
 
-        while (!line.empty()) {
+        while (!inf.empty()) {
             int distanse = 0;
             std::string stop_to_name;
-            distanse = std::stoi(line.substr(0, line.find_first_of("m to ")));
-            line.erase(0, line.find_first_of("m to ") + 5);
-            if (line.find("m to ") == line.npos) {
-                stop_to_name = line.substr(0, line.npos - 1);
-                Stop* to = catalogue.FindBusStop(stop_to_name);
-                catalogue.SetStopDistance(from, to, distanse);
-                if (!catalogue.FindBusStop(to->name)->stop_distances.count(from->name)) {
-                    catalogue.SetStopDistance(to, from, distanse);
+            distanse = std::stoi(inf.substr(0, inf.find_first_of("m to ")));
+            inf.erase(0, inf.find_first_of("m to ") + 5);
+            if (inf.find("m to ") == inf.npos) {
+                stop_to_name = inf.substr(0, inf.npos - 1);
+                infostruct::Stop* to = ctlg.FindBusStop(stop_to_name);
+                ctlg.SetStopDistance(from, to, distanse);
+                if (!ctlg.FindBusStop(to->name)->stop_distances.count(from->name)) {
+                    ctlg.SetStopDistance(to, from, distanse);
                 }
-                line.clear();
+                inf.clear();
             }
             else {
-                stop_to_name = line.substr(0, line.find_first_of(','));
-                Stop* to = catalogue.FindBusStop(stop_to_name);
-                catalogue.SetStopDistance(from, to, distanse);
-                if (!catalogue.FindBusStop(to->name)->stop_distances.count(from->name)) {
-                    catalogue.SetStopDistance(to, from, distanse);
+                stop_to_name = inf.substr(0, inf.find_first_of(','));
+                infostruct::Stop* to = ctlg.FindBusStop(stop_to_name);
+                ctlg.SetStopDistance(from, to, distanse);
+                if (!ctlg.FindBusStop(to->name)->stop_distances.count(from->name)) {
+                    ctlg.SetStopDistance(to, from, distanse);
                 }
-                line.erase(0, line.find_first_of(',') + 2);
+                inf.erase(0, inf.find_first_of(',') + 2);
             }
         }
     }
 }
-
