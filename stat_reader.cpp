309882336e59bf94a2fde8cd05894386af1fe0c0
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-30 20:14:56 +0400

    Update stat_reader.cpp

--- stat_reader.cpp
+++ stat_reader.cpp
@@ -2,50 +2,51 @@
 
 #include <iomanip>
 
-void OutTransport(TransportCatalogue& catalogue) {
-    size_t requests_count;
-    std::cin >> requests_count;
-    for (size_t i = 0; i < requests_count; ++i) {
-        std::string keyword, line;
+void out::Transport(infocatalogueclass::TransportCatalogue& catalogue, std::istream& in) {
+    size_t count;
+    std::cin >> count;
+    for (size_t i = 0; i < count; ++i) {
+        std::string keyword, inf;
         std::cin >> keyword;
-        std::getline(std::cin, line);
+        std::getline(std::cin, inf);
         if (keyword == "Bus") {
-            OutBus(catalogue, line);
+            out::Bus(catalogue, inf, std::cout);
         }
         if (keyword == "Stop") {
-            OutStop(catalogue, line);
+            out::Stop(catalogue, inf, std::cout);
         }
     }
 }
 
-void OutBus(TransportCatalogue& ctlg, std::string inf) {
-    std::string route_number = inf.substr(1, inf.npos);
-    if (ctlg.FindBusRoute(route_number)) {
-        std::cout << "Bus " << route_number << ": " << ctlg.BusRouteInformation(route_number).stops_count << " stops on route, "
-            << ctlg.BusRouteInformation(route_number).unique_stops_count << " unique stops, " << ctlg.BusRouteInformation(route_number).route_length << " route length, "
-            << ctlg.BusRouteInformation(route_number).curvature << " curvature\n";
+void out::Bus(infocatalogueclass::TransportCatalogue& ctlg, const std::string& inf, std::ostream& out) {
+    std::string number = inf.substr(1, inf.npos);
+    if (ctlg.FindBusRoute(number)) {
+        out << "Bus " << number << ": " << ctlg.BusRouteInformation(number).stops_count << " stops on route, "
+            << ctlg.BusRouteInformation(number).unique_stops_count << " unique stops, " << ctlg.BusRouteInformation(number).route_length << " route length, " << ctlg.BusRouteInformation(number).curvature << " curvature\n";
     }
     else {
-        std::cout << "Bus " << route_number << ": not found\n";
+        out << "Bus " << number << ": not found\n";
     }
 }
 
 
-void OutStop(TransportCatalogue& ctlg, std::string inf){
-    if (ctlg.FindBusStop(inf) == nullptr) {
-        std::cout << "Stop " << inf << ": not found" << std::endl;
-    }
-    else {
-        auto a = ctlg.BusToStop(inf);
-        if (a.empty()) {
-            std::cout << "Stop " << inf << ": no buses" << std::endl;
-        }
-        else {
-            std::cout << "Stop " << inf << ": buses ";
-            for (const auto& bus : a) {
+void out::Stop(infocatalogueclass::TransportCatalogue& ctlg, const std::string& inf, std::ostream& out) {
+    std::string stop_name = inf.substr(1, inf.npos);
+    if (ctlg.FindBusStop(stop_name)) {
+        out << "Stop " << stop_name << ": ";
+        std::set<std::string> buses = ctlg.BusToStop(stop_name);
+        if (!buses.empty()) {
+            out << "buses ";
+            for (const auto& bus : buses) {
                 std::cout << bus << " ";
             }
-            std::cout << std::endl;
+            out << "\n";
         }
+        else {
+            out << "no buses\n";
+        }
+    }
+    else {
+        out << "Stop " << stop_name << ": not found\n";
     }
 }
