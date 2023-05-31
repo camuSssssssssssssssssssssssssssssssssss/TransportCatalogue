5af9774878de967a4f2ddd1c30753fce4b426c3c
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-31 22:25:23 +0400

    Update request_handler.cpp

--- request_handler.cpp
+++ request_handler.cpp
@@ -1,1 +1,12 @@
 
+#include "request_handler.h"
+
+std::optional<domain::Bus> RequestHandler::GetBusStat(const std::string_view& bus_name) const
+{
+	return std::optional<domain::Bus>();
+}
+
+const std::unordered_set<domain::Bus>* RequestHandler::GetBusesByStop(const std::string_view& stop_name) const
+{
+	return nullptr;
+}
