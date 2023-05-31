898fc59d394df1a53b0958907fda6c6c1c45c42c
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-31 22:25:04 +0400

    Update request_handler.h

--- request_handler.h
+++ request_handler.h
@@ -1,1 +1,26 @@
 
+#pragma once
+#include "transport_catalogue.h"
+#include "json_reader.h"
+#include <optional>
+
+
+class RequestHandler {
+public:
+    // MapRenderer понадобится в следующей части итогового проекта
+    //RequestHandler(const  infocatalogueclass::TransportCatalogue& db, const renderer::MapRenderer& renderer);
+
+    // Возвращает информацию о маршруте (запрос Bus)
+    std::optional<domain::Bus> GetBusStat(const std::string_view& bus_name) const;
+
+    // Возвращает маршруты, проходящие через
+    const std::unordered_set<domain::Bus>* GetBusesByStop(const std::string_view& stop_name) const;
+
+    // Этот метод будет нужен в следующей части итогового проекта
+    //svg::Document RenderMap() const;
+
+private:
+    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
+    const infocatalogueclass::TransportCatalogue& db_;
+    /*const renderer::MapRenderer& renderer_;*/
+};
