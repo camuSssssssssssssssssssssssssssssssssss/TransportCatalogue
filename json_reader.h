75322e99e4dfdace8ea4ce13ef9502a2b4ec7355
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-31 22:26:33 +0400

    Update json_reader.h

--- json_reader.h
+++ json_reader.h
@@ -1,23 +1,27 @@
 #pragma once
 #include "json.h"
+#include "geo.h"
 #include "transport_catalogue.h"
-#include "map_renderer.h"
+#include "map_render.h"
 #include "domain.h"
-
 
 namespace jReader {
 	class JsonReadInfo {
 	public:
-
+		
 		JsonReadInfo(json::Document input) : input_(input) {}
-
+		
 		json::Node BusReq() const;
 		json::Node StopReq() const;
 
 		void ParseInfo(infocatalogueclass::TransportCatalogue& ctlg);
+		void FillStopDistances(infocatalogueclass::TransportCatalogue& ctlg);
+		std::tuple<std::string, geo::Coordinates, std::map<std::string, int>> FillStop(const json::Dict& request_map) const;
 
 	private:
 		json::Document input_;
 	};
+
+	
 
 }
