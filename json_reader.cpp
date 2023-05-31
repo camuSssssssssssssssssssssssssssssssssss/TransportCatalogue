31a62e349c3d412633f12501cec5b6357b307f22
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-31 22:26:07 +0400

    Update json_reader.cpp

--- json_reader.cpp
+++ json_reader.cpp
@@ -10,9 +10,11 @@ json::Node jReader::JsonReadInfo::BusReq() const {
 json::Node jReader::JsonReadInfo::StopReq() const
 {
 	return json::Node();
+	
 }
 
 void jReader::JsonReadInfo::ParseInfo(infocatalogueclass::TransportCatalogue& ctlg) {
+	
 	domain::Bus bus_info;
 	domain::Stop stop_info;
 	json::Array array = BusReq().AsArray();
@@ -40,45 +42,36 @@ void jReader::JsonReadInfo::ParseInfo(infocatalogueclass::TransportCatalogue& ct
 			geo::Coordinates stop_coordinate = { latitude, longitude };
 			stop_info.name = stop_name;
 			stop_info.coordinates = stop_coordinate;
-			ctlg.AddBusStop(stop_info);
+			ctlg.AddBusStop(stop_info);			
 		}
 	}
-	
 }
 
 
+void jReader::JsonReadInfo::FillStopDistances(infocatalogueclass::TransportCatalogue& ctlg){
+	const json::Array& array = BusReq().AsArray();
+	for (auto& request_stops : array) {
+		const auto& request_stops_map = request_stops.AsMap();
+		const auto& type = request_stops_map.at("type").AsString();
+		if (type == "Stop") {
+			auto [stop_name, coordinates, stop_distances] = FillStop(request_stops_map);
+			for (auto& [to_name, dist] : stop_distances) {
+				auto from = ctlg.FindBusStop(stop_name);
+				auto to = ctlg.FindBusStop(to_name);
+				ctlg.SetStopDistance(from, to, dist);
+			}
+		}
+	}
+}
 
-//infostruct::Stop fill::ParseStop(std::string& inf) {
-//	infostruct::Stop stop;
-//	std::string stop_name = inf.substr(1, inf.find_first_of(':') - inf.find_first_of(' ') - 1);
-//
-//	double latitude = std::stod(inf.substr(inf.find_first_of(':') + 2, inf.find_first_of(',') - 1));
-//	double longitude;
-//	inf.erase(0, inf.find_first_of(',') + 2);
-//	if (inf.find_last_of(',') == inf.npos) {
-//		longitude = std::stod(inf.substr(0, inf.npos - 1));
-//		inf.clear();
-//	}
-//	else {
-//		longitude = std::stod(inf.substr(0, inf.find_first_of(',')));
-//		inf.erase(0, inf.find_first_of(',') + 2);
-//	}
-//	coordinate::Coordinates stop_coordinates = { latitude, longitude };
-//
-//	stop.name = stop_name;
-//	stop.coordinates = stop_coordinates;
-//
-//	return stop;
-//}
-
-//void jReader::JsonReadInfo::PrintInfo() {
-//	json::Array arr = BusReq().AsArray();
-//	for (const auto& ar : arr) {
-//		ar.AsMap().at("name").AsString();
-//	}
-//
-//}
-
-
-
+std::tuple<std::string, geo::Coordinates, std::map<std::string, int>> jReader::JsonReadInfo::FillStop(const json::Dict& request_map) const {
+	std::string stop_name = request_map.at("name").AsString();
+	geo::Coordinates coordinates = { request_map.at("latitude").AsDouble(), request_map.at("longitude").AsDouble() };
+	std::map<std::string, int> stop_distances;
+	auto& distances = request_map.at("road_distances").AsMap();
+	for (auto& [stop_name, dist] : distances) {
+		stop_distances.emplace(stop_name, dist.AsInt());
+	}
+	return std::make_tuple(stop_name, coordinates, stop_distances);
+}
 
