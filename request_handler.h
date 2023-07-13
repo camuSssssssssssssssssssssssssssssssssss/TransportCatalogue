3a9b094208600729c016667d6cbe6720d35cb0a6
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-13 16:49:01 +0400

    Update request_handler.h

--- request_handler.h
+++ request_handler.h
@@ -1,26 +1,35 @@
-
 #pragma once
-#include "transport_catalogue.h"
-#include "json_reader.h"
-#include <optional>
 
+#include "json.h"
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+#include "transport_router.h"
+
+#include <sstream>
 
 class RequestHandler {
 public:
-    // MapRenderer понадобится в следующей части итогового проекта
-    //RequestHandler(const  infocatalogueclass::TransportCatalogue& db, const renderer::MapRenderer& renderer);
+    RequestHandler(const infocatalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const transport_router::Router& router): catalogue_(catalogue), renderer_(renderer), router_(router)
+    {}
 
-    // Возвращает информацию о маршруте (запрос Bus)
-    std::optional<domain::Bus> GetBusStat(const std::string_view& bus_name) const;
+    std::optional<domain::RouteInforamtion> GetBusStat(const std::string_view bus_number) const;
+    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
+    bool IsBusNumber(const std::string_view bus_number) const;
+    bool IsStopName(const std::string_view stop_name) const;
+    const std::optional<graph::Router<double>::RouteInfo> GetRoute(const std::string_view stop_from, const std::string_view stop_to) const;
+    const graph::DirectedWeightedGraph<double>& GetRouterGraph() const;
 
-    // Возвращает маршруты, проходящие через
-    const std::unordered_set<domain::Bus>* GetBusesByStop(const std::string_view& stop_name) const;
+    void ProcessRequests(const json::Node& stat_requests) const;
 
-    // Этот метод будет нужен в следующей части итогового проекта
-    //svg::Document RenderMap() const;
+    const json::Node PrintRoute(const json::Dict& request_map) const;
+    const json::Node PrintStop(const json::Dict& request_map) const;
+    const json::Node PrintMap(const json::Dict& request_map) const;
+    const json::Node PrintRouting(const json::Dict& request_map) const;
+
+    svg::Document RenderMap() const;
 
 private:
-    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
-    const infocatalogueclass::TransportCatalogue& db_;
-    /*const renderer::MapRenderer& renderer_;*/
+    const infocatalogue::TransportCatalogue& catalogue_;
+    const renderer::MapRenderer& renderer_;
+    const transport_router::Router& router_;
 };
