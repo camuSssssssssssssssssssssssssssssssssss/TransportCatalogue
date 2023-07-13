71d0d3ce2964af6496a8a8d96382664dcc174aa0
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-13 16:47:32 +0400

    Update json_reader.h

--- json_reader.h
+++ json_reader.h
@@ -1,27 +1,33 @@
 #pragma once
+
 #include "json.h"
-#include "geo.h"
 #include "transport_catalogue.h"
-#include "map_render.h"
-#include "domain.h"
+#include "map_renderer.h"
+#include "transport_router.h"
+#include "request_handler.h"
 
-namespace jReader {
-	class JsonReadInfo {
-	public:
-		
-		JsonReadInfo(json::Document input) : input_(input) {}
-		
-		json::Node BusReq() const;
-		json::Node StopReq() const;
+#include <iostream>
 
-		void ParseInfo(infocatalogueclass::TransportCatalogue& ctlg);
-		void FillStopDistances(infocatalogueclass::TransportCatalogue& ctlg);
-		std::tuple<std::string, geo::Coordinates, std::map<std::string, int>> FillStop(const json::Dict& request_map) const;
+class JSONReader {
+public:
+    JSONReader(std::istream& input) : input_(json::Load(input)) {
+    }
 
-	private:
-		json::Document input_;
-	};
+    const json::Node& RenderSettings() const;
+    const json::Node& BaseRequests() const;
+    const json::Node& StatRequests() const;
+    const json::Node& RoutingSettings() const;
 
-	
+    void FillCatalogue(infocatalogue::TransportCatalogue&);
+    renderer::MapRenderer FillRenderSettings(const json::Node& settings) const;
+    transport_router::Router FillRoutingSettings(const json::Node& settings) const;
 
-}
+
+private:
+    json::Document input_;
+    json::Node null_ = nullptr;
+
+    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict&) const;
+    void FillStopDistances(infocatalogue::TransportCatalogue&) const;
+    std::tuple<std::string_view, std::vector<const domain::Stop*>, bool> FillRoute(const json::Dict&, infocatalogue::TransportCatalogue&) const;
+};
