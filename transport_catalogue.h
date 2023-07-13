33f232d771b97e61473d6f31110e241bed9b1bbc
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-13 16:50:02 +0400

    Update transport_catalogue.h

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -1,58 +1,40 @@
 #pragma once
 
 #include "geo.h"
+#include "domain.h"
 
+#include <iostream>
 #include <deque>
 #include <string>
-#include <iostream>
 #include <unordered_map>
 #include <vector>
 #include <stdexcept>
+#include <optional>
+#include <unordered_set>
 #include <set>
 #include <map>
-#include <unordered_set>
-#include <functional>
-#include "domain.h"
-#include "geo.h"
 
 
-
-
-namespace infocatalogueclass {
+namespace infocatalogue {
     class TransportCatalogue {
     public:
-
-        
-        struct StopsHasher {
-            size_t operator()(const std::pair<const domain::Stop*, const domain::Stop*>& points) const {
-                size_t hash_first = std::hash<const void*>{}(points.first);
-                size_t hash_second = std::hash<const void*>{}(points.second);
-                return hash_first + hash_second * 37;
-            }
-        };
-
-        void AddBusRoute(const domain::Bus&);
-        void AddBusStop(const domain::Stop&);
-        const domain::Bus* FindBusRoute(const std::string&) const;
-        domain::Stop* FindBusStop(const std::string&) const;
-        const domain::RouteInfo BusRouteInformation(const std::string&) const;
-        size_t UniqueStopsCount(const std::string&) const;
-        std::set<std::string>BusToStop(const std::string&) const;
-        void SetStopDistance(domain::Stop*, domain::Stop*, int);
-        int GetStopDistance(const domain::Stop*, const domain::Stop*) const;
-
-        
+        void AddBusRoute(std::string_view bus_number, const std::vector<const domain::Stop*>& stops, bool circle);
+        void AddBusStop(std::string_view stop_name, const geo::Coordinates coordinates);
+        const domain::Bus* FindBusRoute(std::string_view bus_number) const;
+        const domain::Stop* FindBusStop(std::string_view stop_name) const;
+        size_t UniqueStopsCount(std::string_view bus_number) const;
+        void SetStopDistance(const domain::Stop* from, const domain::Stop* to, const int distance);
+        int GetStopDistance(const domain::Stop* from, const domain::Stop* to) const;
+        const std::map<std::string_view, const domain::Bus*> GetSortedBuses() const;
+        const std::map<std::string_view, const domain::Stop*> GetSortedStops() const;
 
     private:
         std::deque<domain::Bus> buses_;
         std::deque<domain::Stop> stops_;
         std::unordered_map<std::string_view, const domain::Bus*> finderbus_;
-        std::unordered_map<std::string_view, domain::Stop*> finderstop_;
-        std::unordered_map<std::string, std::set<std::string>> bustoforstop_;
-        std::unordered_map<std::pair<const domain::Stop*, const domain::Stop*>, int, StopsHasher> stop_distances;
-
-
-        
+        std::unordered_map<std::string_view, const domain::Stop*> finderstop_;
+        std::unordered_map<std::pair<const domain::Stop*, const domain::Stop*>, int, domain::Hasher> stop_distances_;
 
     };
 }
+
