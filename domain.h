8dd99621cfc10715264b81e70cdf24990af68d95
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-13 16:45:04 +0400

    Update domain.h

--- domain.h
+++ domain.h
@@ -1,41 +1,36 @@
 #pragma once
 #include "geo.h"
-#include <vector>
 #include <string>
+#include <vector>
+#include <set>
 #include <unordered_map>
 
+namespace domain {
 
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+        std::set<std::string> buses_by_stop;
+    };
 
-namespace domain{
+    struct Bus {
+        std::string number;
+        std::vector<const Stop*> stops;
+        bool is_circle;
+    };
 
-        
+    struct RouteInforamtion {
+        size_t stops_count;
+        size_t unique_stops_count;
+        double route_length;
+        double curvature;
+    };
 
-        struct Bus {
-            std::string number;
-            std::vector<std::string> stops;
-            bool circular_route;
-        };
-
-        
-
-        struct Stop {
-            std::string name;
-            geo::Coordinates coordinates;
-        };
-        
-        
-
-
-        
-
-        struct RouteInfo {
-            size_t stops_count;
-            size_t unique_stops_count;
-            double route_length;
-            double curvature;
-        };
-
-        
-
+    struct Hasher {
+        size_t operator()(const std::pair<const Stop*, const Stop*>& points) const {
+            size_t hash_first = std::hash<const void*>{}(points.first);
+            size_t hash_second = std::hash<const void*>{}(points.second);
+            return hash_first + hash_second * 37;
+        }
+    };
 }
-
